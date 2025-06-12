6a946a57f408ff6e22db23de133fb69277fc8563
Artem <122204149+letv1nnn@users.noreply.github.com>
2025-06-12 20:47:49 +0100

    Add files via upload

---
+++ GC/src/gc.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+#include "malloc.h"
+
+
+struct GCObject {
+    bool marked = false;
+    std::vector<GCObject*> references;
+
+    virtual ~GCObject() = default;
+
+    virtual void mark() {
+        if (marked)
+            return;
+        marked = true;
+        for (auto *ref : references)
+            if (ref) ref->mark();
+    }
+};
+
+std::unordered_set<GCObject *> allocated_objects;
+
+// A little bit changed memory allocator from the malloc that I've imlpemented;
+// gc_malloc allocates memory using my own implementation of malloc
+// and casts it to GCObject;
+void *gc_malloc(size_t size) {
+    void *raw = malloc(size);
+    if (!raw) return nullptr;
+    GCObject *obj = reinterpret_cast<GCObject*>(raw); // treat raw memory as GCObject;
+    allocated_objects.insert(obj);
+    return obj;
+}
+
+// insert an already allocated object to alloc objects;
+void gc_register(GCObject* obj) {
+    allocated_objects.insert(obj);
+}
+
+// Executing mark-and-sweep garbage collection
+void gc_collect(const std::vector<GCObject*> &roots) {
+    // Mark step
+    for (auto *root : roots)
+        if (root) root->mark();
+
+    // Sweep step
+    std::unordered_set<GCObject*> survivors;
+    for (auto *obj : allocated_objects) {
+        if (!obj->marked)
+            free(obj);
+        else {
+            obj->marked = false;
+            survivors.insert(obj);
+        }
+    }
+
+    allocated_objects = std::move(survivors);
+}
+
+struct Node : GCObject {
+    int value;
+    Node *next;
+
+    Node (int val)
+        : value(val), next(nullptr) {}
+
+    void mark() override {
+        if (marked) return;
+        marked = true;
+        if (next) next->mark();
+    }
+};
+
+int main() {
+
+    return 0;
+}
+
---
+++ GC/src/malloc.h
@@ -0,0 +1,17 @@
+#ifndef MALLOC_H
+#define MALLOC_H
+
+#include <assert.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define DEBUG_MODE 0
+
+void *malloc(size_t size);
+void free(void *ptr);
+void *calloc(size_t nmemb, size_t size);
+void *realloc(void *ptr, size_t size);
+
+
+#endif
